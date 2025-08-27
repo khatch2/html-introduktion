64e8cd26daabd526489f3e59e657dae2a7c1fb16
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-08-27 10:11:05 +0200

    initial npm demo files

---
+++ 03-javascript/npm-demo/script.js
@@ -0,0 +1,10 @@
+console.log("hej");
+
+/*
+Demo: Använda npm
+• Initiera projekt, npm init
+• Installera ett paket, uuid
+• Om git repo, lägg till .gitignore
+• Använda paket
+• Se hur det påverkar sidan (network)
+*/
