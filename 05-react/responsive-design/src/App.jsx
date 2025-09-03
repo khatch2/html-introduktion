4696abd4f2ff79a05a183b61dd7eca2f2025960c
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-09-03 11:32:37 +0200

    initial project setup

---
+++ 05-react/responsive-design/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Responsive Design</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti, nihil? Enim, vero quia!</p>
+    </>
+  )
+}
+
+export default App
