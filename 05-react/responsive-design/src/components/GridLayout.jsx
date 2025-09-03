0bf3725a5bce23eda2c0e6a4b624a59ab7233bfb
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-09-03 12:56:55 +0200

    format code

--- 05-react/responsive-design/src/components/GridLayout.jsx
+++ 05-react/responsive-design/src/components/GridLayout.jsx
@@ -4,12 +4,13 @@ function GridLayout() {
   return ( 
     <>
       <h2>Tittle</h2>
-    <section className={styles.layout}>
-      <p>Lorem ipsum dolor sit amet.</p>
-      <p>Lorem ipsum dolor sit amet.</p>
-      <p>Lorem ipsum dolor sit amet.</p>
-      <p>Lorem ipsum dolor sit amet.</p>
-    </section>
+      
+      <section className={styles.layout}>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </section>
     </>
    );
 }
