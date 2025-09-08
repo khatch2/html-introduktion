22c0a78c4c741f4c08089c342fb72d8a9609fb51
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-09-08 13:59:38 +0200

    add test for counter

--- 05-react/modern-architecture-example/server/app.test.js
+++ 05-react/modern-architecture-example/server/app.test.js
@@ -10,8 +10,37 @@ describe("GET /api/hello", () => {
   });
 
   it("responds with Hello World!", async () => {
-    const res = await request(app).get("/api/hello");
+    const expectedMsg = "Hello World!";
 
-    expect(res.text).toBe("Hello World!");
+    const res = await request(app).get("/api/hello");
+    const data = JSON.parse(res.text);
+
+    expect(data).toBeTruthy();
+    expect(data).toHaveProperty("msg");
+    expect(data).toHaveProperty("counterHello");
+    expect(data.msg).toBe(expectedMsg);
+  });
+
+  it("responds correct numer of counter for requests", async () => {
+    const res1 = await request(app).get("/api/hello");
+    const data1 = JSON.parse(res1.text);
+
+    expect(data1).toBeTruthy();
+    expect(data1).toHaveProperty("counterHello");
+    expect(data1.counterHello).toBe(3);
+
+    const res2 = await request(app).get("/api/hello");
+    const data2 = JSON.parse(res2.text);
+
+    expect(data2).toBeTruthy();
+    expect(data2).toHaveProperty("counterHello");
+    expect(data2.counterHello).toBe(4);
+
+    const res3 = await request(app).get("/api/hello");
+    const data3 = JSON.parse(res3.text);
+
+    expect(data3).toBeTruthy();
+    expect(data3).toHaveProperty("counterHello");
+    expect(data3.counterHello).toBe(5);
   });
 });
