51f2cbee99c812abbdae08f01125a779f34c6f93
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-09-08 10:05:31 +0200

    add initial server setup

--- 05-react/modern-architecture-example/server/app.js
+++ 05-react/modern-architecture-example/server/app.js
@@ -1,1 +1,15 @@
 // Bygg en “Hello World”-server i Node.js & Express
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/min-endpoint", (req, res) => {
+  res.send("Min nya endpoint");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
